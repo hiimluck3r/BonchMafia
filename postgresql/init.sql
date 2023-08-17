66f62a12d216a6c64d09d4e4d407c0078508c2f1
hiimluck3r <luck3rinc@yandex.ru>
2023-08-17 18:03:35 +0300

    added special service accounts

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -4,3 +4,4 @@ CREATE TABLE games(id SERIAL PRIMARY KEY, gamedate VARCHAR, gamehost VARCHAR, do
 CREATE TABLE users(id SERIAL PRIMARY KEY, userid BIGINT, don BIGINT, don_total BIGINT, mafia BIGINT, mafia_total BIGINT, sheriff BIGINT, sheriff_total BIGINT, citizen BIGINT, citizen_total BIGINT, won BIGINT, lost BIGINT, mentor VARCHAR, nickname VARCHAR);
 
 INSERT INTO users(userid, don, don_total, mafia, mafia_total, sheriff, sheriff_total, citizen, citizen_total, won, lost, mentor, nickname) VALUES (-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'blank', 'blank');
+INSERT INTO users(userid, don, don_total, mafia, mafia_total, sheriff, sheriff_total, citizen, citizen_total, won, lost, mentor, nickname) VALUES (-1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '-', '-');
